80ae60bda210ac6680b6a6ba2e1d8edab41f29ed
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-09-27 16:10:27 +0700

    multiple catch

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -2,12 +2,34 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Exception\NoCookieForYou;
+use AppBundle\Exception\NoCookiesLeft;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller
 {
     public function homepageAction()
     {
         return $this->render('main/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/crazy-dave")
+     */
+    public function cookiesAction()
+    {
+        try {
+            if (random_int(0, 1)) {
+                throw new NoCookieForYou();
+            }
+
+            throw new NoCookiesLeft();
+        } catch (NoCookieForYou | NoCookiesLeft $e) {
+            $whisper = sprintf('Crazy Dave whispered "%s"', $e->getMessage());
+        }
+
+        return new Response(sprintf('<html><body>%s</body></html>', $whisper));
     }
 }
